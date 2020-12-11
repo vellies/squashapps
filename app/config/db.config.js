45a5cf8044902fa0bf5a7666fb67fcfedbc13a38
Vellies <vellies6113@gmail.com>
2020-12-11 17:04:27 +0530

    a

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,5 @@
 module.exports = {
     url: "mongodb+srv://vellies:vellies113.@live01-jjkmg.mongodb.net/squashapps"
   };
+
+  // https://www.freecodecamp.org/news/securing-node-js-restful-apis-with-json-web-tokens-9f811a92bb52/
